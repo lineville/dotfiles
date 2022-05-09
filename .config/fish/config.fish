111b3b9da6699bddf7cdd536a7d173074d9d86f9
Liam Neville <lineville@github.com>
2022-05-09 14:43:45 -0700

    dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+status --is-interactive; and rbenv init - fish | source
+
+set -x GPG_TTY $(tty)
+set -x JAVA_HOME (/usr/libexec/java_home -v 1.8.0_321)
+alias lg="lazygit"
+export PATH="$HOME/.cargo/bin:$PATH"
