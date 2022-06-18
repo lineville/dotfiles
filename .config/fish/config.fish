a3b7aac5d3b30d4255591941d0cef9b5263a04c1
Liam Neville <lineville@github.com>
2022-06-18 14:28:15 -0700

    add more configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,6 @@ status --is-interactive; and rbenv init - fish | source
 set -x GPG_TTY $(tty)
 set -x JAVA_HOME (/usr/libexec/java_home -v 1.8.0_321)
 alias lg="lazygit"
+alias k="kubectl"
+alias l="ls -a"
 export PATH="$HOME/.cargo/bin:$PATH"
